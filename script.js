296a080c4d32f916fd90ce44806861eb2caf4fce
MaxWong03 <madmaxmmajournal@gmail.com>
2019-07-10 16:18:25 -0700

    added click event for X and O

--- script.js
+++ script.js
@@ -1,30 +1,54 @@
 
 
+const domObjects = {
+  cross: '.fas.fa-times.fa-8x',
+  circle: '.far.fa-circle.fa-7x',
+  first: '#first',
+  second: '#second',
+  bigCross: 'fa-10x',
+  bigCircle: 'fa-9x',
+  choice: '.choice',
+  selection: '.selection'
+
+}
 
 
 $(function () {
 
 
+
   //hover or X
-  $('.fas.fa-times.fa-8x').hover(function () {
-    $(this).toggleClass('fa-10x', true);
-    $('#first').show();
-    $('#first').css('font-size', '4em');
+  $(domObjects.cross).hover(function () {
+    $(this).toggleClass(domObjects.bigCross, true);
+    $(domObjects.first).css('font-size', '4em');
   }, function () {
-    $(this).toggleClass('fa-10x', false);
-    $('#first').css('font-size', '0em');
+    $(this).toggleClass(domObjects.bigCross, false);
+    $(domObjects.first).css('font-size', '0em');
   }
   );
 
   //hover or O
-  $('.far.fa-circle.fa-7x').hover(function(){
-    $(this).toggleClass('fa-9x', true);
-    $('#second').css('font-size', '4em');
+  $(domObjects.circle).hover(function(){
+    $(this).toggleClass(domObjects.bigCircle, true);
+    $(domObjects.second).css('font-size', '4em');
   }, function(){
-    $(this).toggleClass('fa-9x', false);
-    $('#second').css('font-size', '0em');
+    $(this).toggleClass(domObjects.bigCircle, false);
+    $(domObjects.second).css('font-size', '0em');
   }
   );
 
+  //Click X
+  $('#X').click(function(){
+    $(domObjects.choice).fadeToggle(500);
+    $(domObjects.circle).fadeToggle(500);
+  });
+
+  //Click O
+  $('#O').click(function(){
+    $(domObjects.choice).fadeToggle(500);
+    $(domObjects.cross).fadeToggle(500);
+    setTimeout(()=>$(domObjects.selection).css('justify-content', 'flex-end'),500);
+    
+  });
 
 })
