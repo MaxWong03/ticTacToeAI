6589ff4041b24fa5a5abf2107d485a2bc6ae8361
MaxWong03 <madmaxmmajournal@gmail.com>
2019-07-11 14:48:29 -0700

    moving x/o to the middle after selection

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+let selected = false;
 
 const domObjects = {
   cross: '.fas.fa-times.fa-8x',
@@ -12,28 +13,36 @@ const domObjects = {
 
 }
 
+const moveToMid = (selector) => {
+  $(selector).css('grid-column-start', 'choiceS');
+  $(selector).css('grid-column-end', 'choiceE');
+};
+
 
 $(function () {
 
+  if(!selected){
+    
+    $(domObjects.cross).hover(function () {
+     $(this).toggleClass(domObjects.bigCross, true);
+     $(domObjects.first).css('font-size', '4em');
+   }, function () {
+     $(this).toggleClass(domObjects.bigCross, false);
+     $(domObjects.first).css('font-size', '0em');
+   }
+   );
+  
+   //hover or O
+   $(domObjects.circle).hover(function(){
+     $(this).toggleClass(domObjects.bigCircle, true);
+     $(domObjects.second).css('font-size', '4em');
+   }, function(){
+     $(this).toggleClass(domObjects.bigCircle, false);
+     $(domObjects.second).css('font-size', '0em');
+   }
+   );
+  } 
   //hover or X
-  $(domObjects.cross).hover(function () {
-    $(this).toggleClass(domObjects.bigCross, true);
-    $(domObjects.first).css('font-size', '4em');
-  }, function () {
-    $(this).toggleClass(domObjects.bigCross, false);
-    $(domObjects.first).css('font-size', '0em');
-  }
-  );
-
-  //hover or O
-  $(domObjects.circle).hover(function(){
-    $(this).toggleClass(domObjects.bigCircle, true);
-    $(domObjects.second).css('font-size', '4em');
-  }, function(){
-    $(this).toggleClass(domObjects.bigCircle, false);
-    $(domObjects.second).css('font-size', '0em');
-  }
-  );
 
   //Click X
   $('#X').click(function(){
@@ -43,9 +52,12 @@ $(function () {
     $(domObjects.circle).animate({
       opacity: 0
     },500);
-
+    setTimeout(()=>{
+      moveToMid(this);
+    }, 600);
+    selected = true;
   });
-
+  
   //Click O
   $('#O').click(function(){
     $(domObjects.choice).animate({
@@ -54,6 +66,10 @@ $(function () {
     $(domObjects.cross).animate({
       opacity:0
     }, 500)
+    setTimeout(()=>{
+      moveToMid(this);
+    },600);
+    selected = true;
   });
 
 })
